8499c6122f17dbd6156fbb233bbff02055aaee32
victor.mukan <vmukan1998@gmail.com>
2019-02-22 15:05:49 +0200

    Add live reload

--- UsersService/src/index.ts
+++ UsersService/src/index.ts
@@ -1,14 +1,35 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
+import * as compress from 'koa-compress';
+import * as koaLogger from 'koa-logger';
+import * as convert from 'koa-convert';
+import * as json from 'koa-json';
+import * as bodyParser from 'koa-bodyparser';
+import * as helmet from 'koa-helmet';
+import * as cors from 'koa-cors';
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/*', async (ctx) => {
+router.get('/', async (ctx) => {
     ctx.body = 'Hello World!';
 });
 
+app.use(helmet());
+app.use(convert(cors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+    headers: 'Content-Type, Authorization'
+})));
+
+app.use(bodyParser());
+app.use(compress());
+app.use(json({ pretty: false, param: 'pretty' }));
+
+app.use(koaLogger());
+
 app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(3000);
 
