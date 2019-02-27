49303a46b2b96107d785a526b0be356aa2bad702
victor.mukan <vmukan1998@gmail.com>
2019-02-27 18:01:13 +0200

    Add sequalize

---
+++ UsersService/src/config/sequelize.ts
@@ -0,0 +1,24 @@
+import * as Sequelize from 'sequelize';
+
+const sequelize = new Sequelize('library', 'postgres', 'root', {
+    host: 'localhost',
+    port: 5000,
+    dialect: 'postgres',
+    operatorsAliases: false,
+
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+});
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
--- UsersService/src/index.ts
+++ UsersService/src/index.ts
@@ -1,4 +1,5 @@
 import * as Koa from 'koa';
+import * as Router from 'koa-router';
 import * as compress from 'koa-compress';
 import * as koaLogger from 'koa-logger';
 import * as convert from 'koa-convert';
@@ -7,10 +8,14 @@ import * as bodyParser from 'koa-bodyparser';
 import * as helmet from 'koa-helmet';
 import * as cors from 'koa-cors';
 
-import router from './config/router';
+import userRouter from './config/router';
+import './config/sequelize';
+
 
 const app = new Koa();
+const router = new Router();
 
+router.use('/user', userRouter.routes());
 
 app.use(helmet());
 app.use(convert(cors({
@@ -18,11 +23,9 @@ app.use(convert(cors({
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
     headers: 'Content-Type, Authorization'
 })));
-
 app.use(bodyParser());
 app.use(compress());
 app.use(json({ pretty: false, param: 'pretty' }));
-
 app.use(koaLogger());
 
 app.use(router.routes());
